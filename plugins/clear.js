4f159a09ec3ec9fb3f96feb3866e33c5ac546037
ABHISHEK SURESH <98966984+BOSSMODZ@users.noreply.github.com>
2022-03-01 00:19:07 +0530

    Update clear.js

--- plugins/clear.js
+++ plugins/clear.js
@@ -22,9 +22,8 @@ async function checkImAdmin(message, user = message.client.user.jid) {
 }
 
 Asena.addCommand({pattern: 'clear', fromMe: true, desc: END, dontAddCommandList: true}, (async (message, match) => {
-
-    await message.sendMessage('```cleaning chat...```');
+    await message.sendMessage('```ABHISHEK SER'S cleaning chat...```');
     await message.client.modifyChat (message.jid, ChatModification.delete);
-    await message.sendMessage('```🏳 Chat cleared 🏳```');
+    await message.sendMessage('```ABHISHEK SER'S 🏳 Chat cleared 🏳```');
 }));
  
